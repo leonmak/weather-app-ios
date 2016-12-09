9c75f1e6040693b4b3c67c012ea27cbdfc5f6a07
leon mak <leonmakansheng@gmail.com>
2016-12-09 11:25:30 +0800

    Basic setup

---
+++ rainyshine/WeatherVC.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherVC.swift
+//  rainyshine
+//
+//  Created by Leon on 9/12/16.
+//  Copyright © 2016 Leon. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        
+    }
+}
